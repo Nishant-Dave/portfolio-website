44eecbd955a94ecfcf489fdc49a99076fbadba47
Nishant-Dave <nishant.nsbda@gmail.com>
2026-03-13 11:20:29 +0530

    update build configs

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Static Directory..."
 mkdir -p staticfiles_build/static
-pip install -r requirements.txt --no-cache-dir
+
+echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
+echo "Build Finished!"
